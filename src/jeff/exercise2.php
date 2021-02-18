8d9e990c106884d4f767017a03fe6cee67def759
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-18 11:44:50 +0100

    :sparkles: add exercise 2

---
+++ src/jeff/exercise2.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" 
+        content="text/html"; charset="utf-8"/>
+    </head>
+    <body>
+    <?php
+        $a = 5;
+        $b = 4;
+        $res = $a + $b;
+        echo "The result of sum $a and $b is $res" 
+    ?>
+    </body>
+</html>
