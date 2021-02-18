e953c7d2abb9e764aac8e0916a344560152beddf
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-18 13:06:23 +0100

    :sparkles: add exercise 3

---
+++ src/jeff/exercise3.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html";
+        charset="utf-8"/>
+        <title>exercise 3</title>
+    </head>
+        <body>
+            <center>
+                <?php
+                    echo phpinfo();
+                ?>
+            </center>
+        </body>
+
+</html>
