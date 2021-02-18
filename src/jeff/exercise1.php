84b780919bfcac3b91b572a5942e27774d0cfe54
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-18 11:44:22 +0100

    :sparkles: add exercise 1

---
+++ src/jeff/exercise1.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html"; charset="utf-8"/>
+        <title>exercise 1</title>
+    </head>
+    <body>
+
+        <?php
+            $start = "Hi ";
+            $end = "to everyone";
+            $all_message = $start.$end;
+            echo $all_message;
+        ?>
+
+    </body>
+
+</html>
