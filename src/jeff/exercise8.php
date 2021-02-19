96084c949319204b7a11dab81bb5b4a6d89b6cfd
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-19 15:42:41 +0100

    add exercise 8

---
+++ src/jeff/exercise8.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html"; 
+        charset="uft-8"/>
+        <title>Exercise 8</title>
+    </head>
+    <body>
+        <?php
+            $keys = array("1" => 90, "30" => 7, "e" => 99, "hola" => 43);
+            foreach($keys as $value) {
+                echo $value."<br>";
+            }
+        ?>
+    </body>
+</html>
