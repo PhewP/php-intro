fd65e6aa7f994000db73fef04ec75484753e3dee
Esperanza <esperanza.canocanalejas@alum.uca.es>
2021-02-20 19:35:55 +0100

    :sparkles: add excercise 2

---
+++ src/hope/ej2.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+    <title>Exercise 2</title>
+    </head>
+
+    <body>
+    <?php
+        $n1 = 6;
+        $n2 = 8;
+        $result = $n1 + $n2;
+        echo $result;
+        echo "<br>";
+        echo "$n1 + $n2 = $result";
+        echo "<br>";
+        echo "La suma de ".$n1." y ".$n2." es ".$result."<br>";
+    ?>
+
+    </body>
+</html>
