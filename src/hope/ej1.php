562d488b05595e2b0aa7803ffa29e8101c4c8fd7
Esperanza <esperanza.canocanalejas@alum.uca.es>
2021-02-20 17:29:05 +0100

    :sparkles: add exercise 1

---
+++ src/hope/ej1.php
@@ -0,0 +1,15 @@
+<hmtl>
+    <head>
+        <title> Exercise 1 </title>
+    </head>
+
+    <body>
+    <?php
+        $first = "hello";
+        $last = "world";
+        $everything = $first.$last;
+        echo $everything;
+    ?>
+
+    </body>
+</html>
