df47fea99e7f0729be2982193ef5ab9ab1b23187
Esperanza <esperanza.canocanalejas@alum.uca.es>
2021-02-20 20:00:15 +0100

    :sparkles: add excercise 4

---
+++ src/hope/ej4.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Excercise 3</title>
+    </head>
+    
+    <body>
+        <?php
+        echo "<table border = 1>";
+        $n = 1;
+        for($i = 1 ; $i <= 10 ; $i++)
+        {
+            echo "<tr>";
+            for($j = 1 ; $j <= 10 ; $j++)
+            {
+                echo "<td>",$n,"</td>";
+                $n = $n + 1;
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+        ?>
+    </body>    
+</html>
