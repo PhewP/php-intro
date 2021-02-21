d6e339696c8d5c1fcfb476accc77093e3899870d
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-21 22:56:22 +0100

    :sparkles: add exercise 12 script

---
+++ src/jeff/exercise12.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta http-equiv="Content-type" content="text/html" charset="utf-8">
+    </head>
+    <body>
+        <?php 
+            $money = $_POST['money']; 
+            $currency = $_POST['conversion'];
+            $money = ($currency == 'pounds') ? $money * 0.86 : $money * 1.21;
+        echo "The conversion is : " . $money . " " . $currency;
+        ?>    
+    </body>
+</html>
