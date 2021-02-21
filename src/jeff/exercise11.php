65af76a1919d00d160887925c680b1a09dd66669
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-21 21:50:38 +0100

    :sparkles: add exercise 11 script

---
+++ src/jeff/exercise11.php
@@ -0,0 +1,12 @@
+<html>
+    <meta http-equiv="content-type" content="text/html"; charset="utf-8"/>
+    <title>Exercise 11</title>
+    <body>
+        <?php 
+            echo "are : ";
+            echo $_POST['euros'] * 0.386;
+            echo " pounds";
+        ?> 
+    </body>
+</html>
+
