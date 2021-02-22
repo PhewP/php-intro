20cb253b519787317e16e0565c30a2db363f4774
jeffresh <manuel.pallanunez@alum.uca.es>
2021-02-22 10:37:03 +0100

    :art: add tab to ajust code in exercise 12 script

--- src/jeff/exercise12.php
+++ src/jeff/exercise12.php
@@ -7,7 +7,7 @@
             $money = $_POST['money']; 
             $currency = $_POST['conversion'];
             $money = ($currency == 'pounds') ? $money * 0.86 : $money * 1.21;
-        echo "The conversion is : " . $money . " " . $currency;
+            echo "The conversion is : " . $money . " " . $currency;
         ?>    
     </body>
 </html>
