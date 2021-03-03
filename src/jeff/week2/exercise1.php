ff5c5ac41d1587f6ba8ec649e0c1f62359755735
jeffresh <manuel.pallanunez@alum.uca.es>
2021-03-03 11:32:22 +0100

    :lipstick:  add style css to exercise 1 week 2

--- src/jeff/week2/exercise1.php
+++ src/jeff/week2/exercise1.php
@@ -1,7 +1,8 @@
 <html>
     <head>
-        <meta http-equiv="Content-type" content="text/hmlt" charset="utf-8">
+        <meta http-equiv="Content-type" content="text/html" charset="utf-8">
         <title> Exercise 1</title>
+        <link rel="stylesheet" href="style.css" type="text/css">
     </head>
 
     <body>
