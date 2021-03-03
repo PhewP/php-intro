7579122de883721d7c41e90dc43cc01a79f4cdea
jeffresh <manuel.pallanunez@alum.uca.es>
2021-03-03 12:19:47 +0100

    :sparkles: add script to create db table votos

---
+++ src/jeff/week2/sql_scripts/votos.sql
@@ -0,0 +1,6 @@
+CREATE TABLE votos (
+    id tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT,
+    votos1 int(10) UNSIGNED NOT NULL DEFAULT '0',
+    votos2 int(10) UNSIGNED NOT NULL DEFAULT '0',
+    primary key (id)
+);
