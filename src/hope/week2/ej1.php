d865dd7a663cbf535d2645ae6a25364d21386de7
Esperanza <esperanza.canocanalejas@alum.uca.es>
2021-03-04 20:04:14 +0100

    :sparkles: add excercise1 php - week 2

---
+++ src/hope/week2/ej1.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Excercise 1</title>
+    </head>
+    
+    <body>
+        <h1>News:</h1>
+        <?php
+        //Connect to server
+            $connection = mysqli_connect("localhost", "cursophp", "lindavista") or die ("Cannot connect to server");
+
+        //Consult de DB
+        ?> 
+    </body>
+</html>
