d4ddd68fa30078fc6a2c1b82b66046efbbd24ac1
Esperanza <esperanza.canocanalejas@alum.uca.es>
2021-03-06 12:14:20 +0100

    :construction: add some code

--- src/hope/week2/ej1.php
+++ src/hope/week2/ej1.php
@@ -7,9 +7,18 @@
         <h1>News:</h1>
         <?php
         //Connect to server
-            $connection = mysqli_connect("localhost", "cursophp", "lindavista") or die ("Cannot connect to server");
+            $connection = mysqli_connect("localhost", "cursophp", "lindavista") 
+            or die ("Cannot connect to server");
 
-        //Consult de DB
+        //DB query
+            $instruction = "select * from lindavista";
+            $query = mysqli_query($connection, $instruction)
+            or die ("Query failure");
+
+        //Show query result
+            $nrows = mysqli_num_rows($query);
+            if($nrows > 0)
+                
         ?> 
     </body>
 </html>
