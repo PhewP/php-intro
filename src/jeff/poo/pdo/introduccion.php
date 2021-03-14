3472ac0c2c2635acf894baa6dd5ba5f0dc502dd7
jeffresh <manuel.pallanunez@alum.uca.es>
2021-03-14 10:13:55 +0100

    :sparkles: add pdo fetch

--- src/jeff/poo/pdo/introduccion.php
+++ src/jeff/poo/pdo/introduccion.php
@@ -14,3 +14,56 @@ Es orientado a objetos aunque el funcionamiento no es muy distinto al de mysqli.
         $base = null; //cierra la conexión
     }
 ?>
+
+<!-- Consultas no preparadas -->
+    <!-- leer datos -->
+    <!-- Método para ejecutra una consulta SQL es : query() -->
+    <!-- método para saber el numero de instancias es rowCount() -->
+    <!-- forma parte del objecto resultado que el método query ha devuelto -->
+    <!-- Ejemplo obtener el numero de registros en la tabla Persona -->
+
+<?php    
+    try{
+        $base = new PDO('mysql:host=127.0.0.1;dbname=prueba_pdo', 'root', 'a6zYXjc6YhWm94xR');
+        $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Recuperacion de datos de la tabla Persona
+        $resultado = $base->query('SELECT * FROM Persona');
+        echo "Numero de personas: ".$resultado->rowCount();
+        // Muestra el número de registros
+
+        $resultado->closeCursor();
+    }catch(Exception $e) {
+        // mensaje en caso de error
+        die('Error :'.$e->getMessage());
+    }
+?>
+
+<!-- Para mostrar los datos de la tabla Persona debe utilizar fetch, que permite leer el registro actual
+y desplazarse al siguiente registro. -->
+<!-- para realizar el fetch de la consulta es: fetch() -->
+<!-- este metodo forma parte del objeto $resultado que el metodo query() ha devuelto -->
+
+<?php 
+    try {
+        $base = new PDO('mysql:host=127.0.0.1;dbname=prueba_pdo', 'root', 'a6zYXjc6YhWm94xR');
+        $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Recuperacion de datos de la tabla persona
+        $resultado = $base->query('SELECT nombre, apellidos FROM persona');
+
+        echo "Numero de personas".$resultado->rowCount();
+        // Mostrar el número de registros
+        while ($datos = $resultado->fetch()) {
+?>
+        <p>
+            Apellidos: <?php echo $datos['apellidos'];?>,
+            Nombre:<?php echo $datos['nombre'];?>
+        </p>
+<?php
+        }
+        $resultado->closeCursor();// Cierre de la consulta
+    }catch(Exception $e) {
+        die("Error :".$e->getMessage());
+    }
+?>
